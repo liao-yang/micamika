b3cd859ff5e215f76fe70c131e4e0b9a7cc72bfe
Your Name <you@example.com>
2018-06-26 20:51:55 +0800

    2018.06.26

--- src/js/allCakes.js
+++ src/js/allCakes.js
@@ -1,3 +1,13 @@
 require(["config"],function(){
-	require(["load"]);
+	require(["jquery","template","load"],function($,template){//短名称的顺序和函数中的金额对应
+		//将模板信息添加进页面中
+		$(function(){
+			$.getJSON("/mock/cakeTemp.json",function(data){
+				//渲染模板
+				const html = template("cakeTemp",{list:data.res_body.list});
+				//添加进网页中
+				$("#cakes").prepend(html);
+			});
+		});
+	});	
 });
--- src/js/config.js
+++ src/js/config.js
@@ -1,7 +1,8 @@
 require.config({
 	baseUrl:"/",
 	paths:{
-		jquery:"lib/jquery/jquery-1.12.4.min",//相对于index.html
-		load:"js/load"
+		jquery:"/lib/jquery/jquery-1.12.4.min",
+		load:"/js/load",
+		template:"/lib/artTemplate/template-web"
 	}
 });
